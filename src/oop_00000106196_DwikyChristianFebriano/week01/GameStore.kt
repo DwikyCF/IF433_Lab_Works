c8ae9788828042fd5f351c5d0b10e8c9aa24dd99
dwiky <dwiky.christian@student.umn.ac.id>
2026-02-06 16:03:26 +0700

    week01: (tugas 2) setup GameStore file

---
+++ src/oop_00000106196_DwikyChristianFebriano/week01/GameStore.kt
@@ -0,0 +1,8 @@
+package oop_00000106196_DwikyChristianFebriano.week01
+
+fun main() {
+    // Langkah 1: Deklarasi variabel menggunakan val
+    val gameTitle = "Elden Ring Shadow of the Erdtree"
+    val price = 799000
+}
+

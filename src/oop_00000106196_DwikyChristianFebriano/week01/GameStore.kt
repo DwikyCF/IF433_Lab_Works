2367177be76f55894fc16a2007795a65d5c49723
dwiky <dwiky.christian@student.umn.ac.id>
2026-02-06 16:11:30 +0700

    week01: (tugas 2) implement receipt printing

--- src/oop_00000106196_DwikyChristianFebriano/week01/GameStore.kt
+++ src/oop_00000106196_DwikyChristianFebriano/week01/GameStore.kt
@@ -6,7 +6,14 @@ fun main() {
 
     val discount = calculateDiscount(price)
     val finalPrice = price - discount
+
+    printReceipt(title = gameTitle, finalPrice = finalPrice)
 }
 
 fun calculateDiscount(price: Int) = if (price > 500_000) (price * 0.2).toInt() else (price * 0.1).toInt()
 
+fun printReceipt(title: String, finalPrice: Int) {
+    println("=== Struk Pembelian SteamKW ===")
+    println("Judul Game : $title")
+    println("Harga Akhir: Rp $finalPrice")
+}

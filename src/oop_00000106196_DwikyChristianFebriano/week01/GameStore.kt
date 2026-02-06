3224eda15b32b145dc4f86d07631c92238efcd1b
dwiky <dwiky.christian@student.umn.ac.id>
2026-02-06 16:05:54 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000106196_DwikyChristianFebriano/week01/GameStore.kt
+++ src/oop_00000106196_DwikyChristianFebriano/week01/GameStore.kt
@@ -1,8 +1,12 @@
 package oop_00000106196_DwikyChristianFebriano.week01
 
 fun main() {
-    // Langkah 1: Deklarasi variabel menggunakan val
     val gameTitle = "Elden Ring Shadow of the Erdtree"
     val price = 799000
+
+    val discount = calculateDiscount(price)
+    val finalPrice = price - discount
 }
+
+fun calculateDiscount(price: Int) = if (price > 500_000) (price * 0.2).toInt() else (price * 0.1).toInt()
 

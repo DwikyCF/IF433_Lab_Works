175d55fffde4dea7c04269f15cb2ae12ed94317d
dwiky <dwiky.christian@student.umn.ac.id>
2026-02-06 14:41:16 +0700

    week01: refactor to use val and string templates

--- src/oop_00000106196_DwikyChristianFebriano/week01/GradingSystem.kt
+++ src/oop_00000106196_DwikyChristianFebriano/week01/GradingSystem.kt
@@ -1,9 +1,8 @@
 package oop_00000106196_DwikyChristianFebriano.week01
 
 fun main() {
-    var name: String = "John Thor"
-    var score: Int = 80
+    val name: String = "John Thor"
+    val score: Int = 80
 
-    // Concatenation gaya lama
-    println("Nama: " + name + ", Nilai: " + score)
+    println("Nama: $name , Nilai: $score")
 }

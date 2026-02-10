ccb909dd9172cf0b4f42b106e76e86f945adeb92
dwiky <dwiky.christian@student.umn.ac.id>
2026-02-10 11:31:29 +0700

    week02: challenge refactor primary constructor using default args

--- src/oop_00000106196_DwikyChristianFebriano/week02/Student.kt
+++ src/oop_00000106196_DwikyChristianFebriano/week02/Student.kt
@@ -3,7 +3,8 @@ package oop_00000106196_DwikyChristianFebriano.week02
 class Student(
     val name: String,
     val nim: String,
-    var major: String
+    var major: String,
+    var gpa: Double = 0.0
 )
 
 {

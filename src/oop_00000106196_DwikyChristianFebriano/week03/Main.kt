82f94ddd67958c655ff88ec32c979f234bc37c2a
dwiky <dwiky.christian@student.umn.ac.id>
2026-02-18 10:27:12 +0700

    week03: test all encapsulation logic in main

--- src/oop_00000106196_DwikyChristianFebriano/week03/Main.kt
+++ src/oop_00000106196_DwikyChristianFebriano/week03/Main.kt
@@ -2,6 +2,15 @@ package oop_00000106196_DwikyChristianFebriano.week03
 
 fun main() {
     val e = Employee("Budi")
-    println("--- Test Error ---")
-    e.salary = 5000
+    println("--- Test Validasi Gaji ---")
+    e.salary = -1000
+    e.salary = 5000000
+    println("Gaji Akhir: ${e.salary}")
+
+    println("\n--- Test Enkapsulasi ---")
+    e.increasePerformance() // Rating naik jadi 4
+    e.printStatus()
+
+    println("\n--- Test Computed Property ---")
+    println("Pajak yang harus dibayar (10%): ${e.tax}")
 }

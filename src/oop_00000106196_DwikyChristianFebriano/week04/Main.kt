dc13ef8d0dccdb81d5588310de505dc5ca9eaad5
dwiky <dwiky.christian@student.umn.ac.id>
2026-02-24 19:52:03 +0700

    week04: (task 2) test Employee hierarchy in main

--- src/oop_00000106196_DwikyChristianFebriano/week04/Main.kt
+++ src/oop_00000106196_DwikyChristianFebriano/week04/Main.kt
@@ -1,6 +1,6 @@
 package oop_00000106196_DwikyChristianFebriano.week04
 
-fun main() {
+    /*fun main() {
     println("--- Testing Vehicle ---")
     val generalVehicle = Vehicle(brand = "Sepeda Onthel")
     generalVehicle.honk()
@@ -16,6 +16,19 @@ fun main() {
     val tesla = ElectricCar(brand = "Tesla", numberOfDoors = 2, batteryCapacity = 100)
     tesla.openTrunk()
     tesla.honk()
-    tesla.accelerate()
+    tesla.accelerate()*/
+
+fun main() {
+    println("\n--- Testing Employee Hierarchy ---")
+
+    // 1. Instansiasi objek Manager
+    val manager = Manager(name = "Dwiky", baseSalary = 5000000)
+    manager.work()
+    println("Bonus Manager: Rp ${manager.calculateBonus()}")
+
+    // 2. Instansiasi objek Developer
+    val dev = Developer(name = "Chaychin", baseSalary = 5000000, programmingLanguage = "Kotlin")
+    dev.work()
+    println("Bonus Developer: Rp ${dev.calculateBonus()}")
 
 }

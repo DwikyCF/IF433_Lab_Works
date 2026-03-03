f3fa7a281d267f7af07073cc04909a5f8fee9c21
dwiky <dwiky.christian@student.umn.ac.id>
2026-03-04 05:58:58 +0700

    week05: (task 1) test MathHelper overloading in main

--- src/oop_00000106196_DwikyChristianFebriano/week05/Main.kt
+++ src/oop_00000106196_DwikyChristianFebriano/week05/Main.kt
@@ -1,6 +1,6 @@
 package oop_00000106196_DwikyChristianFebriano.week05
 
-fun main() {
+/*fun main() {
     val dosen1 = Dosen(nama = "Pak Alex", nidn = "0123456")
     val admin1 = Admin(nama = "Bu Siti")
 
@@ -22,4 +22,15 @@ fun main() {
         }
         println("----------------------------------------")
     }
+}*/
+
+fun main () {
+    println("\n=== TESTING MATH HELPER  ===")
+    val math = MathHelper() // Instansiasi objek MathHelper
+
+    println("Luas Persegi (sisi = 5) : ${math.hitungLuas(5)}")
+
+    println("Luas Persegi Panjang (Panjang = 5 , Lebar = 10): ${math.hitungLuas(5, 10)}")
+
+    println("Luas Lingkaran (Jari Jari = 7.0): ${math.hitungLuas(7.0)}")
 }

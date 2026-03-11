ed3d2ecbfcbfa07be6e760e610311d6353db6e28
dwiky <dwiky.christian@student.umn.ac.id>
2026-03-11 10:34:06 +0700

    week06: fix diamond problem using explicit super delegation

--- src/oop_00000106196_DwikyChristianFebriano/week06/Smartphone.kt
+++ src/oop_00000106196_DwikyChristianFebriano/week06/Smartphone.kt
@@ -1,4 +1,9 @@
 package oop_00000106196_DwikyChristianFebriano.week06
 
 class Smartphone : Camera, Phone {
+    override fun turnOn() {
+        super<Camera>.turnOn() // Menjalankan logika Camera
+        super<Phone>.turnOn()  // Menjalankan logika Phone
+        println("Sistem operasi Smartphone berhasil booting.")
+    }
 }

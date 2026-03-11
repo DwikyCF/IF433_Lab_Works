a109b0ebbe8b576ae83b9a8e2ac255ac793445ca
dwiky <dwiky.christian@student.umn.ac.id>
2026-03-11 10:29:30 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000106196_DwikyChristianFebriano/week06/GadgetInterfaces.kt
@@ -0,0 +1,9 @@
+package oop_00000106196_DwikyChristianFebriano.week06
+
+interface Camera {
+    fun turnOn() { println("Lensa kamera terbuka dan sensor aktif.") }
+}
+
+interface Phone {
+    fun turnOn() { println("Sinyal seluler mencari jaringan.") }
+}

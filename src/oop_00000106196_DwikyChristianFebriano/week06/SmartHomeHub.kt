30ae4d5c889f05ffe6f12947a0920b198aca7016
dwiky <dwiky.christian@student.umn.ac.id>
2026-03-11 11:09:35 +0700

    week06: (task) implement advanced smart casting in Hub

---
+++ src/oop_00000106196_DwikyChristianFebriano/week06/SmartHomeHub.kt
@@ -0,0 +1,33 @@
+package oop_00000106196_DwikyChristianFebriano.week06
+
+class SmartHomeHub {
+    val devices = mutableListOf<SmartDevice>()
+
+    fun addDevice(device: SmartDevice) {
+        devices.add(device)
+        println("Perangkat '${device.name}' berhasil ditambahkan ke Hub.")
+    }
+
+    fun turnOffAllSwitches() {
+        println("\n--- MEMATIKAN SEMUA PERANGKAT ---")
+        for (device in devices) {
+            if (device is Switchable) {
+                device.turnOff()
+            }
+        }
+    }
+
+    fun activateSecurityMode() {
+        println("\n--- MENGAKTIFKAN MODE KEAMANAN! ---")
+        for (device in devices) {
+
+            if (device is Recordable) {
+                device.startRecord()
+            }
+
+            if (device is SmartSpeaker) {
+                device.playMusic("Sirine Peringatan")
+            }
+        }
+    }
+}

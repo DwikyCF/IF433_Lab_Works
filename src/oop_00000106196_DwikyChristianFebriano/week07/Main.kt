80ac0f346d56b44005a27a789efe0ec2bdc5eb85
dwiky <dwiky.christian@student.umn.ac.id>
2026-03-25 22:30:31 +0700

    week07: (trial) trigger non-exhaustive when compiler error

--- src/oop_00000106196_DwikyChristianFebriano/week07/Main.kt
+++ src/oop_00000106196_DwikyChristianFebriano/week07/Main.kt
@@ -26,6 +26,14 @@ package oop_00000106196_DwikyChristianFebriano.week07
         val (userName, userAge) = data1
         println("Destructured: $userName berumur $userAge")
 
+        println("\n=== TEST SEALED CLASS ===")
+        val response: ApiResponse = ApiResponse.Success("Data berhasil ditarik!")
+
+        val uiMessage = when(response) {
+            is ApiResponse.Success -> "Tampilkan: ${response.data}"
+            is ApiResponse.Error -> "Munculkan alert: ${response.message}"
+        }
+
 
     }
 

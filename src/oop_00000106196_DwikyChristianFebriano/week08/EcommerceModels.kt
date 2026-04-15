750aadefc9dbe7f6a564e496de2ccffbfdfce7db
dwiky <dwiky.christian@student.umn.ac.id>
2026-04-15 09:43:20 +0700

    week08: (task) create sealed class Product and data classes

---
+++ src/oop_00000106196_DwikyChristianFebriano/week08/EcommerceModels.kt
@@ -0,0 +1,7 @@
+package oop_00000106196_DwikyChristianFebriano.week08
+
+sealed class Product
+
+data class Electronic(val id: String, val name: String, val warrantyMonths: Int) : Product()
+
+data class Clothing(val id: String, val name: String, val size: String) : Product()

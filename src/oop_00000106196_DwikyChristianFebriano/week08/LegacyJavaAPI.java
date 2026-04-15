72d7ac5794f031447ac0163b9ef581f608e21c20
dwiky <dwiky.christian@student.umn.ac.id>
2026-04-15 09:18:51 +0700

    week08: create LegacyJavaAPI class in Java

---
+++ src/oop_00000106196_DwikyChristianFebriano/week08/LegacyJavaAPI.java
@@ -0,0 +1,7 @@
+package oop_00000106196_DwikyChristianFebriano.week08;
+
+public class LegacyJavaAPI {
+    public static String fetchServerStatus() {
+        return "SERVER_ONLINE_200_OK";
+    }
+}

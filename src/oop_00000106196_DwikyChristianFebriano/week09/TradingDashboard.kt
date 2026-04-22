aeed895a59c82a6c49b648562045cfe0cc1b3258
dwiky <dwiky.christian@student.umn.ac.id>
2026-04-22 09:34:20 +0700

    week09: (task) print worst performers and unique pairs

--- src/oop_00000106196_DwikyChristianFebriano/week09/TradingDashboard.kt
+++ src/oop_00000106196_DwikyChristianFebriano/week09/TradingDashboard.kt
@@ -32,8 +32,11 @@ fun main() {
     println("\n--- TOP PERFORMERS (WIN) ---")
     topPerformersString.forEach { println(it) }
 
+    println("\n--- WORST PERFORMERS (LOSS) ---")
+    worstPerformersString.forEach { println(it) }
 
-
+    println("\n--- UNIQUE TRADING PAIRS ---")
+    println(uniquePairs)
 
 }
 

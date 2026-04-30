7730b3126f3866550d5791717928932ce0b8f3f4
dwiky <dwiky.christian@student.umn.ac.id>
2026-04-30 08:32:06 +0700

    week10: (task) populate coin repository

--- src/oop_00000106196_DwikyChristianFebriano/week10/CryptoDashboard.kt
+++ src/oop_00000106196_DwikyChristianFebriano/week10/CryptoDashboard.kt
@@ -2,4 +2,7 @@ package oop_00000106196_DwikyChristianFebriano.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+    coinRepo.add(Coin("BTC", 0.85))
+    coinRepo.add(Coin("ETH", 12.5))
+    coinRepo.add(Coin("USDT", 5000.0))
 }

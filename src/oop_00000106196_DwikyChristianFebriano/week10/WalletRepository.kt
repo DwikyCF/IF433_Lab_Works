551c70bdffc107567359dcc3cb1604b99d137dff
dwiky <dwiky.christian@student.umn.ac.id>
2026-04-30 08:22:25 +0700

    week10: (task) implement generic WalletRepository

---
+++ src/oop_00000106196_DwikyChristianFebriano/week10/WalletRepository.kt
@@ -0,0 +1,17 @@
+package oop_00000106196_DwikyChristianFebriano.week10
+
+class WalletRepository<T> {
+    private val items = mutableListOf<T>()
+
+    fun add(item: T) {
+        items.add(item)
+    }
+
+    fun getAll(): List<T> {
+        return items
+    }
+
+    fun search(predicate: (T) -> Boolean): List<T> {
+        return items.filter(predicate)
+    }
+}

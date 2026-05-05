443b7d9c96a01e034b072603ae88c563008860f5
dwiky <dwiky.christian@student.umn.ac.id>
2026-05-05 16:31:29 +0700

    week11: create string extension functions

---
+++ src/oop_00000106196_DwikyChristianFebriano/week11/StringExtensions.kt
@@ -0,0 +1,9 @@
+package oop_00000106196_DwikyChristianFebriano.week11
+
+fun String.addGreeting(): String {
+    return "Hello, $this"
+}
+
+fun String.repeatTimes(n: Int): String {
+    return this.repeat(n)
+}
